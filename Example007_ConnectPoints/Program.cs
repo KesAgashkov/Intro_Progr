ba116a6cb2b377196ac39c4b7c3d8ba263de3378
Kostya <kostilio911@gmail.ru>
2022-09-15 20:31:04 +0400

    wrong example

---
+++ Example007_ConnectPoints/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Clear();
+//Console.SetCursorPosition(10, 4);
+//Console.WriteLine ('+');
+
+Console.SetCursorPosition(xa, yb);
+Console.WriteLine("+");
+
+int x = xa, y = xb;
+
+int count = 0;
+
+while (count < 10)
+{
+    int what = new Random().Next(0, 3); //3 не входит
+    if (what == 0)
+    {
+        x = (x + xa) / 2;
+        y = (y + ya) / 2;
+    }
+    if (what == 1)
+    {
+        x = (x + xb) / 2;
+        y = (y + yb) / 2;
+    }
+     if (what == 2)
+    {
+        x = (x + xc) / 2;
+        y = (y + yc) / 2;
+    }
+    Console.SetCursorPosition(x,y);
+    Console.WriteLine ("+");
+    count++;
+}
+
+
