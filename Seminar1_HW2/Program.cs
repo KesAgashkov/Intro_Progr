c94d6e6b41f403700b2eff3640d7ffd26809fb50
Kostya <kostilio911@gmail.ru>
2022-09-17 11:00:24 +0400

    example search max from 3 number

---
+++ Seminar1_HW2/Program.cs
@@ -0,0 +1,19 @@
+﻿int a = new Random().Next(1, 101);
+int b = new Random().Next(1, 101);
+int c = new Random().Next(1, 101);
+
+if ((a > c) || (a > b))
+{
+    Console.WriteLine("max number a = " + a + " last numbers : " + b + "; " + c);
+}
+else if (b > c)
+{
+    Console.WriteLine("max number b = " + b + " last numbers : " + a + "; "  + c);
+}
+else
+{
+    Console.WriteLine("max number c = " + c + " last numbers : " + a + "; " + b);
+}
+// не стабильно рабатает , иногда не выдаёт нужные значения, не могу понять по какой причине.
+
+
