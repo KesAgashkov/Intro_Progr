d6046c0d4adfb23ad32c913bf1cce95ca016044c
Kostya <kostilio911@gmail.ru>
2022-09-17 11:23:17 +0400

    example search odd or even num

---
+++ Seminar1_HW3/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.WriteLine("Введите любое целое число от 1 до 100");
+int num = int.Parse (Console.ReadLine());
+
+if ( num % 2 == 0 )
+{
+    System.Console.WriteLine ("Ура ваше число " + num + " - четное число");
+}
+else
+{
+    System.Console.WriteLine ("Сожалею " + num + " - нечетное число");
+}
