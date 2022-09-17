88014f70b84a00fc86189815ee93050fcb2bfec7
Kostya <kostilio911@gmail.ru>
2022-09-17 10:09:20 +0400

    add example search max and min num

---
+++ Seminar1_HW1/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = new Random().Next(1, 11);
+int b = new Random().Next(1, 11);
+if (a > b)
+{
+    System.Console.WriteLine("max a = " + a + "; min b = " + b);
+}
+else
+{
+    System.Console.WriteLine("max b = " + b + "; min a = " + a);
+}
+System.Console.WriteLine ("Хорошего дня");
+
+
